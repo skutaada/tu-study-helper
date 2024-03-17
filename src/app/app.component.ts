c50dae58a951ab11ddb5422268f57bf69172917e
Adam Skuta <adam.skuta@gmail.com>
2024-03-17 16:18:22 +0100

    Testing tiss web scraping

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,26 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'tu-study-helper';
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+    this.getTissPrograms().subscribe(data => {
+      console.log(data);
+    });
+  }
+
+  getTissPrograms() {
+    return this.http.get('https://tiss.tuwien.ac.at/curriculum/studyCodes.xhtml')
+  }
 }
